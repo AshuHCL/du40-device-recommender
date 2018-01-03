b13bdee44ffc4d4b14573867f443e19e3519407c
AshuHCL <ashutosh.p@hcl.com>
2018-01-03 22:01:12 +0530

    [DU40 POC]: Updated the device-tiles component and device recommender service to get the recommended devices and their prices dynamically from REX Api,and catalog Api respectively. HTML binding is to be done next. Also added a json response for catalog rex response.

--- src/app/common/services/device-recommender.service.ts
+++ src/app/common/services/device-recommender.service.ts
@@ -5,7 +5,11 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 @Injectable()
 export class DeviceRecommenderService {
 	public baseUrl: string = 'https://www.att.com/';
-  	public upgradingUserApiUrl: string = '../assets/content/upgrading-user-info.json';
+  	private upgradingUserApiUrl: string = '../assets/content/upgrading-user-info.json';
+  	private catalogRexResponse: string = '../assets/content/recommended-devices.json';
+  	private deviceRecommenderApi: string = 'https://www.att.com/rex/recommendation/rexBySku/';
+  	private deviceImageUrlRoot = 'https://www.att.com/catalog/en/xpress/'
+  	private catalogApi = 'https://www.att.com/services/shopwireless/model/att/ecom/api/CatalogServiceController/catalogService';
   	
   	constructor(private http: HttpClient) { }
   	
@@ -26,5 +30,70 @@ export class DeviceRecommenderService {
             upDeviceType = 'device';
         }
         return upDeviceType;
+    };
+
+    public getRecommendedDevices = function (upgradingDeviceDetails) {
+        var rex = this.deviceRecommenderApi,
+            params = this.createParams(upgradingDeviceDetails);
+
+        rex = rex + upgradingDeviceDetails.deviceSkuId;
+        return this.http.get(rex, params);
+    };
+
+    public getRecommendationsFromCatalog = function () {
+    	return this.http.get(this.catalogRexResponse, {});
+    };
+
+    public getItemPriceData = function(skuId) {
+    	return this.http.get(this.catalogApi, {
+            params: {
+                actionType: 'getskuprices',
+                skuId: skuId
+            },
+            spinner: true
+        });
+    };
+
+    public getXpressImagePath = function (manufacturer, shortDisplayName, displayName, color, extension) {
+        var manufacturerClean = this.getCleanString(manufacturer);
+        var nameClean = this.getCleanString(shortDisplayName || displayName);
+        var colorClean = this.getCleanString(color);
+
+        return this.deviceImageUrlRoot
+            + manufacturerClean + '/'
+            + nameClean + '/'
+            + colorClean + extension;
+    };
+
+    private getCleanString = function(dirty) {
+        var anythingThatDoesntMatch = /[^0-9a-zA-Z&+_ -]/g;
+        var leadingAndTrailingSpaces = /^\s+|\s+$/g;
+        var replacement = '_';
+        var nothing = '';
+        var clean;
+
+        clean = dirty || 'unknown';
+        clean = clean.replace(anythingThatDoesntMatch, replacement);
+        clean = clean.replace(leadingAndTrailingSpaces, nothing);
+
+        return clean;
+    }
+
+    private createParams = function (upgradingDeviceDetails) {
+        var params = {
+            params: {
+                profile: 'DeviceUpgrade',
+                filter: ['type:device', 'type:displayByGrp'],
+                exclude: ['type:removeDuplicateSibling', 'flowTypes:up'],
+                plancode: upgradingDeviceDetails.planSocCode,
+                sdgid: upgradingDeviceDetails.sharedDataGroupSocCode,
+                // REX needs this flag to expose special flair flags for each recommendation
+                showDisplayContent: 'true'
+            },
+            timeout: 2000,
+            spinner: true
+        };
+
+        return params;
     };
 }
--- src/app/device-recommender/device-tiles/device-tiles.component.ts
+++ src/app/device-recommender/device-tiles/device-tiles.component.ts
@@ -1,15 +1,101 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { DeviceRecommenderService } from '../../common/services/device-recommender.service';
 
 @Component({
-  selector: 'device-tiles',
-  templateUrl: `device-tiles.component.html`,
-  styleUrls: [`device-tiles.component.less`]
+    selector: 'device-tiles',
+    templateUrl: `device-tiles.component.html`,
+    styleUrls: [`device-tiles.component.less`],
+  	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DeviceTilesComponent implements OnInit {
+	
+	public itemPrice;
+	public commitmentTerms = [];
+	public imgUrl = [];
+	public totalDeviceRecommendations;
+	public totalDevicesLoaded;
+	public rexTrueRecommendations = 0;
+	public initDevicesLoaded = 6;
 
-  constructor() { }
+	private getDevicePriceDetails = function (rexDevices) {
 
-  ngOnInit() {
-  }
+        var x, skuids;
+		this.totalDeviceRecommendations = rexDevices;
+
+		//gets the initial recommended devices to be displayed from recommender API
+        for (x in this.totalDeviceRecommendations) {
+            if (this.totalDeviceRecommendations[x].source === 'rex') {
+                this.rexTrueRecommendations += 1;
+            }
+        }
+        if (this.rexTrueRecommendations < this.initDevicesLoaded / 2 || this.rexTrueRecommendations >= this.initDevicesLoaded) {
+            if (this.totalDevicesLoaded > this.totalDeviceRecommendations.length) {
+                this.totalDevicesLoaded = this.totalDeviceRecommendations.length;
+            }
+            // $scope.viewMoreDevices($scope.initDevicesLoaded);
+        } else {
+            this.totalDevicesLoaded = this.rexTrueRecommendations;
+            // $scope.viewMoreDevices(rexTrueRecommendations);
+        }
+
+        skuids = this.totalDeviceRecommendations.map(function (item) {
+            return item.skuId;
+        }).join(',');
+
+        this.deviceRecommenderService.getItemPriceData(skuids).subscribe((result:any) => {
+            var commitmentTerms = {};
+            this.itemPrice = result.payload;
+
+            for (let key in this.itemPrice) {
+            	var commitmentTerm = {name:"",leaseTotalMonths:"",deviceType:""};
+                commitmentTerm.name = this.itemPrice[key].contractType;
+                commitmentTerm.leaseTotalMonths = this.itemPrice[key].contractLength;
+                commitmentTerm.deviceType = this.itemPrice[key].deviceType;
+                commitmentTerms[key] = commitmentTerm;
+                this.imgUrl[key] = this.deviceRecommenderService.getXpressImagePath(
+                        this.itemPrice[key].brand, null, this.itemPrice[key].shortDisplayName, this.itemPrice[key].color, '-hero.png');
+            };
+            this.commitmentTerms = commitmentTerms;
+            // this.fetchShortLegalContentForDevices(this.commitmentTerms); TODO
+        });
+	};
+
+	private fetchShortLegalContentForDevices = function(commitmentTerms) {
+		// TODO
+	};
+
+    constructor(
+    	private deviceRecommenderService: DeviceRecommenderService,
+        private ref: ChangeDetectorRef 
+    ) { }
+
+    ngOnInit() {
+  	  	this.deviceRecommenderService.getUpgradingDeviceDetailsData().subscribe( 
+        	(data: any) => {
+        		this.deviceRecommenderService.getRecommendedDevices(data.payload).subscribe( 
+                    recommendedDevices => {
+                    	if(recommendedDevices == undefined || !recommendedDevices) {
+                    		this.deviceRecommenderService.getRecommendationsFromCatalog().subscribe( 
+                    			(recommendedDevicesFromCatalog: any) => {
+		                    		this.getDevicePriceDetails(recommendedDevicesFromCatalog);
+                    			}
+                    		);
+                    	} else {
+	                    	this.getDevicePriceDetails(recommendedDevices);
+                    	}
+		            },
+				    err => {
+				      	console.log('Something went wrong!');
+				      	this.deviceRecommenderService.getRecommendationsFromCatalog().subscribe( 
+            				(recommendedDevicesFromCatalog: any) => {
+		                    	this.getDevicePriceDetails(recommendedDevicesFromCatalog);
+            				}
+            			);
+				    }
+		        );
+    		}
+    	);
+    }
 
 }
