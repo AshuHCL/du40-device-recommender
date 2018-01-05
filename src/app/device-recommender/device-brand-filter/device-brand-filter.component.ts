0bc152b9398bc4e765765161cb6f96eaf92f0d36
masroor545 <masroor545@gmail.com>
2018-01-05 22:50:20 +0530

    [DU40] filter code by masroor

--- src/app/device-recommender/device-brand-filter/device-brand-filter.component.ts
+++ src/app/device-recommender/device-brand-filter/device-brand-filter.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { DeviceRecommenderService } from '../../common/services/device-recommender.service';
 @Component({
   selector: 'device-brand-filter',
   templateUrl: `device-brand-filter.component.html`,
-  styleUrls: [`device-brand-filter.component.less`]
+  styleUrls: [`device-brand-filter.component.less`],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DeviceBrandFilterComponent implements OnInit {
+  hideElem: boolean;
+  isClassVisible: false;
+public totalDeviceRecommendations;
 
-  constructor() { }
+private getDevicePriceDetails = function (rexDevices) {
+		this.totalDeviceRecommendations = rexDevices;
+		this.ref.detectChanges();
+	};
+  constructor(private ref: ChangeDetectorRef ) { this.hideElem = false;}
 
   ngOnInit() {
-    // TODO
+    var num = [0,1,2,3,4,5,6,7];
+    this.getDevicePriceDetails(num);
   }
 
 }
